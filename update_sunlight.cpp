c9fa99bf6841e28f9680f7f9d0d0f123d531f36f
npecko <5827002+npecko@users.noreply.github.com>
2019-11-01 17:27:14 +0300

    Revert "Created accumulating resource for the player"
    This reverts commit 7cb4eb6aa71c710f11ffa1b5c960179cf78f2ee9.

--- update_sunlight.cpp
+++
@@ -1,9 +0,0 @@
-double startTime = GetTickCount();
-while (player.getStatus()) //player is a premade object of Player class, works while player is alive.
-{
-	double currentTime = GetTickCount() - startTime;
-	if (currentTime >= 10000) {  //10 seconds.
-		this->updateSunlight(10); //updated by +10
-		startTime = GetTickCount();
-	}
-}
