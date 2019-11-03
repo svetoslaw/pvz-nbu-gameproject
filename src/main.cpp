cef01e8bfb7c2ce93f8220a1cf9fab7379888da3
npecko <5827002+npecko@users.noreply.github.com>
2019-11-03 18:55:22 +0300

    Create Player class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include "SDL.h"
+#include "iostream"
+#include "Windows.h"
+#include "Player.h"
 
 int main(int argc, char* args[])
 {
+	Player player = Player();
+	double startTime = GetTickCount();
+	while (player.getStatus() != false) {
+		double currentTime = GetTickCount() - startTime;
+		if (currentTime >= 10000) {  //10 seconds.
+			player.updateSunlight(10); //updated by +10
+			startTime = GetTickCount();
+		}
+		
+		//Rest of code goes here
+
+	}
+
 	return 0;
 }
