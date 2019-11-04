0fb1a3434db2a29af886a5b6eec20709e5e91c00
npecko <5827002+npecko@users.noreply.github.com>
2019-11-05 00:15:24 +0300

    Add Main window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
 #include "SDL.h"
+#include <iostream>
+#include "Player.h"
+
+using namespace std;
+
+SDL_Window* window;
+SDL_Renderer* renderer;
 
 int main(int argc, char* args[])
 {
+	//Main Window
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+	window = SDL_CreateWindow(
+		"Flora vs. Undead",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		1280,
+		720,
+		SDL_WINDOW_SHOWN
+	);
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	//Main Loop
+	SDL_Event event;
+	bool running = true;
+	while (running) 
+	{
+		while (SDL_PollEvent(&event)) 
+		{
+			if (event.type == SDL_QUIT) 
+			{
+				running = false;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
