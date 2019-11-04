9cc07a2e55c7d39cd9101cfe50723535abe16fdd
npecko <5827002+npecko@users.noreply.github.com>
2019-11-05 01:50:24 +0300

    Added background img
    Add placeholder background img to Main Menu https://prnt.sc/pshjzm

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 SDL_Window* window;
 SDL_Renderer* renderer;
 
-int main(int argc, char* args[])
+int main(int argc, char* argv[])
 {
 	//Main Window
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
@@ -20,28 +20,35 @@ int main(int argc, char* args[])
 		SDL_WINDOW_SHOWN
 	);
 
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
 	//Main Loop
 	SDL_Event event;
 	bool running = true;
-	while (running) 
+	while (running)
 	{
-		while (SDL_PollEvent(&event)) 
+		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_QUIT) 
+			if (event.type == SDL_QUIT)
 			{
 				running = false;
 				break;
 			}
 		}
+		renderer = SDL_CreateRenderer(window, -1, 0);
+
+		SDL_Texture* main_menu_bg;
+		SDL_Surface* temp_surface = SDL_LoadBMP("assets/fvz_main_menu.bmp");
+		main_menu_bg = SDL_CreateTextureFromSurface(renderer, temp_surface);
+		SDL_FreeSurface(temp_surface);
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, main_menu_bg, 0, 0);
+		SDL_RenderPresent(renderer);
+
 	}
 
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
+
 }
